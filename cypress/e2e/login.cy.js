5afd0f981fd2924457b90b35fe7749be0f1d8c0a
souzareverton <souzareverton@gmail.com>
2024-04-01 23:47:25 -0300

    corrigindo falhas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,18 @@
 describe('Login', () => {
   it('successfully log in', () => {
+    let emailAddress = process.env.USER_EMAIL
+    let password = process.env.USER_PASSWORD
+
+    if (!emailAddress || !password){
+      emailAddress = Cypress.env('USER_EMAIL')
+      password = Cypress.env('USER_PASSWORD')
+    }
+
     cy.intercept('GET', '**/notes').as('getNotes')
 
     cy.visit('/login')
-    cy.get('#email').type(Cypress.env('USER_EMAIL'))
-    cy.get('#password').type(Cypress.env('USER_PASSWORD'))
+    cy.get('#email').type(emailAddress)
+    cy.get('#password').type(password)
     cy.contains('button', 'Login').click()
 
     cy.contains('h1', 'Your Notes').should('be.visible')
--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -2,8 +2,14 @@ import { faker } from '@faker-js/faker/locale/en'
 
 describe('Sign up', () => {
   it('successfully signs up using confirmation code sent via email', () => {
-    const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
-    const password = Cypress.env('USER_PASSWORD')
+
+    let emailAddress = process.env.MAILOSAUR_SERVER_ID
+    let password = process.env.USER_PASSWORD
+
+    if (!emailAddress || !password){
+      emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
+      password = Cypress.env('USER_PASSWORD')
+    }
 
     cy.fillSignUpFormAndSubmit(emailAddress,password)
     cy.contains('h1', 'Your Notes').should('be.visible')
