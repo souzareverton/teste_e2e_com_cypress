f5d8a0865a93fc9eecf76351ea2fcc5579645546
souzareverton <souzareverton@gmail.com>
2024-04-02 00:10:55 -0300

    corrigindo falhas de novo

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -3,13 +3,13 @@ import { faker } from '@faker-js/faker/locale/en'
 describe('Sign up', () => {
   it('successfully signs up using confirmation code sent via email', () => {
 
-    let emailAddress = process.env.MAILOSAUR_SERVER_ID
-    let password = process.env.USER_PASSWORD
+    let emailAddress = process.env.CYPRESS_MAILOSAUR_SERVER_ID1 || `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
+    let password = process.env.CYPRESS_USER_PASSWORD1 || Cypress.env('USER_PASSWORD')
 
-    if (!emailAddress || !password){
-      emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
-      password = Cypress.env('USER_PASSWORD')
-    }
+    // if (!emailAddress || !password){
+    //   emailAddress =s
+    //   password =
+    // }
 
     cy.fillSignUpFormAndSubmit(emailAddress,password)
     cy.contains('h1', 'Your Notes').should('be.visible')
